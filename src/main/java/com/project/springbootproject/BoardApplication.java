15ece082c47b27496a03e515a5fdb6dcd5c223b4
조현준 <hyunjun3753@naver.com>
2022-10-25 17:10:25 +0900

    수정, 삭제 미구현

--- src/main/java/com/project/springbootproject/BoardApplication.java
+++ src/main/java/com/project/springbootproject/BoardApplication.java
@@ -2,12 +2,12 @@ package com.project.springbootproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class SpringbootProjectApplication {
-
+public class BoardApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootProjectApplication.class, args);
+        SpringApplication.run(BoardApplication.class, args);
     }
-
 }
